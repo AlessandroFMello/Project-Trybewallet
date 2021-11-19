3947ef6c72140910a930fe394d48a20bf6c8e418
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-11-19 19:48:15 -0300

    Adiciona um header na pagina /carteira que contenha o email e despesas cotadas na moeda escolhida pelo usuario

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,40 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currencies: [],
+      expenses: []
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { getEmail } = this.props;
+    return (
+      <header className="wallet-header">
+        <h1>Trybe</h1>
+        <div className="email-currency-component">
+          <h3 data-testid="email-field">{ `Email: ${getEmail}` }</h3>
+          <div>
+            <h3 data-testid="total-field">Despesa total: R$ 0.00</h3>
+            <h3 data-testid="header-currency-field">BRL</h3>
+          </div>
+        </div>
+      </header>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  getEmail: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  getEmail: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
